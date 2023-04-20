86058be5c45a89d395323d5e800852db21f72b2a
Frednguyen0000 <fred.nguyen.0000@gmail.com>
2023-04-19 21:36:26 -0700

    Mortaria Alpha Build 8.0
    -Updated UI
    -Added Subscription Plans
    -Added Dark Mode
    -Bug Fixes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let contrastToggle = false;
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if(contrastToggle) {
+        document.body.classList += " dark-theme";
+        console.log(contrastToggle);
+    }
+    else{
+        document.body.classList.remove ("dark-theme");
+        console.log(contrastToggle);
+    }
+}
